400a4c213462766956c1226ab1405a778c4c00ee
sibintphilip <sibinphilip@gmail.com>
2025-04-27 20:48:38 +0000

    Remove Chess Club from in-memory activity database

--- src/app.py
+++ src/app.py
@@ -21,12 +21,6 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 
 # In-memory activity database
 activities = {
-    "Chess Club": {
-        "description": "Learn strategies and compete in chess tournaments",
-        "schedule": "Fridays, 3:30 PM - 5:00 PM",
-        "max_participants": 12,
-        "participants": ["michael@mergington.edu", "daniel@mergington.edu"]
-    },
     "Programming Class": {
         "description": "Learn programming fundamentals and build software projects",
         "schedule": "Tuesdays and Thursdays, 3:30 PM - 4:30 PM",
